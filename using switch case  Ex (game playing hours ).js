7c5a6abcb1d8bed18dec89f39b674cb9736fb8a4
karthikmarella24 <80272758+karthikmarella24@users.noreply.github.com>
2021-05-30 13:30:15 +0530

    Add files via upload

---
+++ using switch case  Ex (game playing hours ).js
@@ -0,0 +1,25 @@
+let gameplayingtimes= "Forza Horizon"
+
+switch(gameplayingtimes){
+
+    case "pubg":
+    console.log( "4 hours")
+    break;
+
+    case "call of duty":
+        console.log( "2 hours")
+        break;
+    case "fortnite":
+        console.log( "2 hours")
+        break;
+
+    case "Red Dead Redemption":
+        console.log( "6 hours")
+
+    case "Forza Horizon":
+        console.log( "4 hours")
+        break;
+
+    default:
+        console.log( "study")
+}
